1ac45017f5cc85b5ab420e6805d02b6c89780a12
Your Name <youremail@domain.com>
2019-06-02 13:27:49 +0530

    Extending Api

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -12,5 +12,6 @@ namespace DatingApp.API.Data
 
         public DbSet<Value> values{get;set;}
         public DbSet<User> Users { get; set; }
+        public Dbset<Photo> Photos { get; set; }
     }
 }
